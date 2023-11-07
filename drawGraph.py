72c9c57ca4fcfbe92f2191eb89bab9b69c0a66d7
Alexey <86290623+AlexeyDate@users.noreply.github.com>
2023-11-07 21:00:23 +0300

    Visual tool

---
+++ drawGraph.py
@@ -0,0 +1,68 @@
+import matplotlib.pyplot as plt
+
+fileNames = ['1.txt', '2.txt', '4.txt', '8.txt']
+colors = ['navy', 'blue', 'turquoise', 'aqua']
+
+threadPoints = [[] for _ in range(len(fileNames))]
+threadTime = [[] for _ in range(len(fileNames))]
+
+fig = plt.figure(figsize=(11, 7))
+plt.ylabel('time (sec)')
+plt.xlabel('number of points')
+
+for i, currentFileName in enumerate(fileNames):
+    with open(currentFileName, 'r') as file:
+        for line in file.readlines():
+            points, time = line.split(' ')
+            threadPoints[i].append(int(points))
+            threadTime[i].append(round(float(time.replace('\n', '')), 2))
+
+    plt.plot(threadPoints[i], threadTime[i], marker='o', color=colors[i], label=str(2 ** i) + ' thread')
+
+visiblePoints = []
+for point in threadPoints[0]:
+    if point % 200 == 0:
+        visiblePoints.append(point)
+    else:
+        visiblePoints.append('')
+
+plt.xticks(ticks=threadPoints[0], labels=visiblePoints)
+plt.grid(visible=1, linestyle="--", linewidth=0.5, color="0.5")
+plt.legend(loc='upper left')
+plt.show()
+fig.savefig('TimeUp.png', bbox_inches='tight')
+
+
+# Calculating Speed Up
+fig = plt.figure(figsize=(11, 7))
+plt.ylabel('speed up (Tserial / Tparallel)')
+plt.xlabel('number of points')
+
+for i in range(len(threadTime)):
+    if i > 0:  # skip single thread
+        speedUp = [threadTime[0][j] / threadTime[i][j] for j in range(len(threadTime[0]))]
+        plt.plot(threadPoints[i], speedUp, marker='o', color=colors[i], label=str(2 ** i) + ' thread')
+
+plt.xticks(ticks=threadPoints[0], labels=visiblePoints)
+plt.grid(visible=1, linestyle="--", linewidth=0.5, color="0.5")
+plt.legend(loc='upper left')
+plt.show()
+fig.savefig('SpeedUp.png', bbox_inches='tight')
+
+
+# Calculating Efficiency
+fig = plt.figure(figsize=(11, 7))
+plt.ylabel('efficiency (Tserial / (Tparallel * p))')
+plt.xlabel('number of points')
+
+for i in range(len(threadTime)):
+    if i > 0:  # skip single thread
+        p = 2 ** i
+        speedUp = [threadTime[0][j] / (threadTime[i][j] * p) for j in range(len(threadTime[0]))]
+        plt.plot(threadPoints[i], speedUp, marker='o', color=colors[i], label=str(p) + ' thread')
+
+plt.xticks(ticks=threadPoints[0], labels=visiblePoints)
+plt.grid(visible=1, linestyle="--", linewidth=0.5, color="0.5")
+plt.legend(loc='upper left')
+plt.show()
+fig.savefig('Efficiency.png', bbox_inches='tight')
